d8f5071561e3b9d9e1836ceedef19af7b57ca259
Prasad Kankhar <prasadkankhar5@gmail.com>
2025-05-16 01:58:30 +0530

    Fix manifest.json, add icons, and update PWA config 22222

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,19 @@
-{
-  "extends": ["react-app", "react-app/jest", "plugin:prettier/recommended"],
-  "rules": {
-    "react/react-in-jsx-scope": "off"
-  }
-}
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    // Add more custom rules here if needed
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
