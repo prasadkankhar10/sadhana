7cb022b9397cc79a51b1ab8e0a78c20400d692ac
Prasad Kankhar <prasadkankhar5@gmail.com>
2025-07-05 00:47:20 +0530

    test fix 2

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFiles: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
   transform: {
     '^.+\\.(ts|tsx)$': ['ts-jest', {
       tsconfig: 'tsconfig.json',
--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,3 @@
-require('@testing-library/jest-dom');
-
 // Jest setup file to mock window.matchMedia for jsdom
 if (typeof window !== 'undefined') {
   window.matchMedia =
